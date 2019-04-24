6b6bb91584f66fc418af1c2a6862c7aa0ed0d865
Umar--Khan <umarkhandev@gmail.com>
2019-04-24 15:05:05 +0100

    done

--- src/index.js
+++ src/index.js
@@ -4,3 +4,30 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+function selectNode(element, string) {
+  return document.querySelector(element).textContent = string;
+}
+
+let authors;
+
+url = "https://randomuser.me/api/"
+
+fetch(url)
+  .then((resp) => resp.json()) 
+    .then(function (data) {
+      authors = data.results[0]; 
+      })
+
+document.querySelector(".btn.btn-primary").addEventListener("click", event => {
+selectNode("#fullname", (authors.name.title + " " + authors.name.first + " " + authors.name.last));
+selectNode("#email", (authors.email));
+selectNode("#phone", (authors.phone));
+selectNode("#cell", (authors.cell));
+selectNode("#date_of_birth", (authors.dob.date));
+selectNode("#street", (authors.location.street));
+selectNode("#city", (authors.location.city));
+selectNode("#state", (authors.location.state));
+selectNode("#postcode", (authors.location.postcode));
+})
+  

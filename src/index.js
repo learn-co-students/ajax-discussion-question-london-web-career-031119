b9a97f1b070b05f0ef6c66477ded86046faa98a2
github username <github email address>
2019-04-24 14:14:38 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,27 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+let person;
+
+fetch('https://randomuser.me/api/')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    person = data.results[0]
+  })
+
+const button = document.querySelector(".btn.btn-primary")
+
+button.addEventListener('click', event => {
+  document.querySelector("#fullname").textContent = person.name.title + " " + person.name.first + " " + person.name.last
+  document.querySelector("#email").textContent = person.email
+  document.querySelector("#street").textContent = person.location.street
+  document.querySelector("#city").textContent = person.location.city
+  document.querySelector("#state").textContent = person.location.state
+  document.querySelector("#postcode").textContent = person.location.postcode
+  document.querySelector("#phone").textContent = person.phone
+  document.querySelector("#cell").textContent = person.cell
+  document.querySelector("#date_of_birth").textContent = person.dob.date.slice(0,10)
+})

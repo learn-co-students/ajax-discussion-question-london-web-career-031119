8994af5f431022d1dcc6b57a715fb351375e65c3
Manon Jacquin <manon.jacquin@gmail.com>
2019-04-24 15:36:35 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -7,50 +7,38 @@ const postcode = document.querySelector('#postcode')
 const phone = document.querySelector('#phone')
 const cell = document.querySelector('#cell')
 const dateOfBirth = document.querySelector('#date_of_birth')
+const image = document.querySelector('#image')
 
 const btn = document.querySelector('button');
 
 const url = 'https://randomuser.me/api/'
 
+btn.addEventListener("click", (event) => {
+  event.preventDefault()
+  fetchData();
+});
 
 function fetchData(){
     fetch(url)
     .then( res => res.json() )
     .then( function(data) {
-      let streetData = data.results[0]['location']['street'];
-      let cityData = data.results[0]['location']['city'];
-      let stateData = data.results[0]['location']['state'];
-      let postCodeData = data.results[0]['location']['postcode'];
-      let phoneData = data.results[0]['phone'];
-      let cellData = data.results[0]['cell'];
-      let nameData = `${data.results[0]['name']['first']} ${data.results[0]['name']['last']}`
-      let emailData = data.results[0]['email'];
-      let dobData = moment(data.results[0]['dob']['date']).format("MMMM Do YYYY");
 
-
-      fullname.innerHTML = nameData;
-      street.innerHTML = streetData;
-      city.innerHTML = cityData;
-      state.innerHTML = stateData;
-      email.innerHTML = emailData;
-      postcode.innerHTML = postCodeData;
-      phone.innerHTML = phoneData;
-      cell.innerHTML = cellData;
-      dateOfBirth.innerHTML = dobData;
+      image.src = data.results[0]['picture']['large']
+      street.innerHTML = data.results[0]['location']['street'];
+      city.innerHTML = data.results[0]['location']['city'];
+      state.innerHTML = data.results[0]['location']['state'];
+      postcode.innerHTML = data.results[0]['location']['postcode'];
+      phone.innerHTML = data.results[0]['phone'];
+      cell.innerHTML = data.results[0]['cell'];
+      fullname.innerHTML = `${data.results[0]['name']['first']} ${data.results[0]['name']['last']}`
+      email.innerHTML = data.results[0]['email'];
+      dateOfBirth.innerHTML = moment(data.results[0]['dob']['date']).format("MMMM Do YYYY");
 
     })
     .catch(function(error) {
       console.log(error);
     });  
 }
-
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
-
-
-btn.addEventListener("click", (event) => {
-  alert('button is clicked')
-  fetchData();
-});
 
 
 

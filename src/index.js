4bc0acd2e93d5bfd26d5e0d106cad432d50a7abc
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-05-01 10:33:00 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+function fetchData() {
+  fetch('https://randomuser.me/api/')
+  .then( res => { return res.json(); } )
+};
+
+const data = fetchData();
+
 const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+});
+
+document.querySelector('.btn-primary').addEventListener("click", () => {
+  console.log("clicked");
 });
